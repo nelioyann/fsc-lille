07b4ee26e8fd11f300905e6ac9ca01c433763595
Yannick Nana <nanayannick1998@gmail.com>
2022-02-16 19:21:01 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact, useIonRouter } from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact, useIonRouter, IonLoading  } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 
